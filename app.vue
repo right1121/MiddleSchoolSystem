9584c8620db0b8412bc8786f39057dc55b0ba49e
MsakiHoshino <right1121@hotmail.co.jp>
2022-12-11 20:57:11 +0900

    lint修正

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@ const { data } = await useFetch('/api/school-lunch')
 <template>
   <div>
     <ul>
-      <li v-for="d in data">{{ d }}</li>
+      <li v-for="d in data" :key="d.value">
+        {{ d }}
+      </li>
     </ul>
     <NuxtWelcome />
   </div>
