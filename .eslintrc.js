6a4162a5af1182dd62ec2eebe902519b7a43bb5c
MsakiHoshino <right1121@hotmail.co.jp>
2022-12-11 20:57:01 +0900

    parser変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,15 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     '@nuxtjs/eslint-config-typescript'
   ],
   overrides: [
   ],
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
