c9b75f7c9f6580402a1f85a932b409d7801297a2
Takahiro Kawata <sincegim0211@gmail.com>
2023-03-22 21:18:50 +0900

    スタッシュ

--- server/api/school-lunch.post.ts
+++ server/api/school-lunch.post.ts
@@ -1,4 +1,4 @@
-import { list } from '~~/usecase/schoolLunch'
+import { list } from '~~/server/usecase/schoolLunch'
 import schoolLunchRepository from '~~/server/infra/schoolLunch'
 
 export default defineEventHandler(() => {
